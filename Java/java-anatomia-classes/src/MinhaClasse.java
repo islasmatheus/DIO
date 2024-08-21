4b9af1d82f10605aa138e723824e929be79d1e3f
islasmatheus <devislasmatheus@gmail.com>
2024-08-21 11:12:22 -0300

    Atualizado

--- Java/java-anatomia-classes/src/MinhaClasse.java
+++ Java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,9 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
+        String primeiroNome = "Islas";
+        String segundoNome = "Matheus";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
-        System.out.print("Olá turma, sejam bem-vindos");
+        System.out.println(nomeCompleto);
+    }
 
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
