4a008cc0fa453f5e36fe55cf210d057fe4df5949
islasmatheus <devislasmatheus@gmail.com>
2024-08-29 11:23:34 -0300

    Atualizado

---
+++ Java/desafio/Candidatura/src/ProcessoSeletivo2.java
@@ -0,0 +1,43 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo2 {
+
+    public static void main(String[] args) {
+        selecaocandidatos();
+    }
+
+    static void selecaocandidatos() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA",
+                "JORGE" };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000;
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATO");
+        }
+    }
+}
