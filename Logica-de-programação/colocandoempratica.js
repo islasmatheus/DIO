e175075781b97c79dcdca313f27afaff6988f03a
Islas Matheus <devislasmatheus@gmail.com>
2025-04-29 10:03:39 -0300

    pratica

---
+++ Logica-de-programação/colocandoempratica.js
@@ -0,0 +1,12 @@
+// Verifique o padrão camelCase das variáveis e constantes.
+
+let poteCafe = "Café Pilão"
+let poteAcucar = "Açucar Cistal"
+let poteBiscoito = "Biscoito Maizena"
+const menssagemDaVovo = "Na cozinha da vovó hoje tem: "
+
+//Estava caro o café pilão. Alterando uma varável no programa.
+
+poteCafe = "Café 3 Corações"
+
+console.log(menssagemDaVovo + poteCafe + " - " + poteAcucar + " - " + poteBiscoito)
