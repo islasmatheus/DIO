d4858f752262ac19a7ec6aaf1a1b09e49e8b7a7a
Islas Matheus <devislasmatheus@gmail.com>
2025-04-29 10:59:17 -0300

    Matriz

---
+++ Logica-de-programação/Matriz.js
@@ -0,0 +1,7 @@
+let nomesPokemon = ["Pikachu" , "Charmander" , "Bulbassaur"]
+let timePokemon = [
+    ["Pikachu", "M" , 1],
+    ["Charmander" , "F" , 4]
+]
+
+console.log("O pokemon "+timePokemon[1][0]+ " é do sexo "+ timePokemon[1][1]+ " e possui level "+ timePokemon[1][2])
