df9c189037de5f8024fafde21da63321e719a50e
Islas Matheus <devislasmatheus@gmail.com>
2025-04-29 10:27:54 -0300

    exercicio 1

---
+++ Logica-de-programação/exercicio1.js
@@ -0,0 +1,9 @@
+//nome , idade, número de telefone, cpf, endereço.
+//srtring, number, boolean e recebe pensão?
+
+let nomePessoa = "Josicreide do Pythinho"
+let idade = 115
+let numeroTelefone = "11-984713388"
+let cpf = "312.122.312-45"
+let endereco = "Rua dos Limoeiros, número 75, Bairro da Jaguatirica"
+let recebePensao = true
