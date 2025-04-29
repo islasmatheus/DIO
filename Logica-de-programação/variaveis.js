07776ecfa8f535ebf2645f9231564150ba1e7302
Islas Matheus <devislasmatheus@gmail.com>
2025-04-29 09:37:38 -0300

    Variaveis

---
+++ Logica-de-programação/variaveis.js
@@ -0,0 +1,10 @@
+// Isso aqui embaixo é um comando de saída
+console.log("Digite o nome do seu jogador: ")
+
+//Declarando a váriavel, parte da memória do programa que salva uma informação para utilizá-lo durante o resto do programa.Pote.
+
+let nickname = "Islas"
+
+//Concatenamos uma mensagem fixa e uma varável.
+console.log("Bem vindo " + nickname)
+console.log(nickname + " entrou no servidor")
