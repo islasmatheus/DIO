9a2425ad17d4e03d137a07176a922a1c044a1934
Islas Matheus <devislasmatheus@gmail.com>
2025-04-29 10:14:27 -0300

    Tipos de Variáveis

---
+++ Logica-de-programação/TiposDeVariaveis.js
@@ -0,0 +1,9 @@
+//Pokemon
+
+let nomePokemon = "Pikachu" //string: texto
+let nivelPokemon = 20 //number: númerico
+let pontosDeVida = 45 //number: númerico
+let pokemonSexo = "M" //string: texto
+let selecionavel = true //booleanas: lógicas
+
+//javascript é uma linguagem de programação não tipada. Não preciso declara se é string, int, boolean.
