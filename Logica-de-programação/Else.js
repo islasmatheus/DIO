6bc6889e9a8a51cef918252bf1a495d82d6a1c24
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 10:37:27 -0300

    If e Else

---
+++ Logica-de-programação/Else.js
@@ -0,0 +1,12 @@
+let possuiOvos = false
+let itensComprados = ""
+
+if (possuiOvos) {
+    itensComprados = "Leite"
+}
+else {
+    console.log("Passou na sessão de congelados")
+    itensComprados = "Lasanha congelada"
+}
+
+console.log(itensComprados)
---
+++ Logica-de-programação/If.js
@@ -0,0 +1,11 @@
+let ehLigado = false
+
+if(ehLigado){
+    console.log("Executou comando")
+}
+
+ehLigado = true
+
+if(ehLigado){
+    console.log("Executou comando")
+}
