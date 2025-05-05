2ddc9fdc3570866b6fcd0a137d99080c4cb27d90
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 09:27:23 -0300

    Operadores

---
+++ Logica-de-programação/OperadooresAritimeticos.js
@@ -0,0 +1,31 @@
+//soma
+let idade = 30
+console.log("Valor da minha variável: " + idade)
+idade = 30 + 6
+console.log("Valor da adição: " + idade)
+
+//subtração
+idade = 30 - 6
+console.log("Valor da subtração: " + idade)
+let primeiroNumero = 1023
+let segundoNumero = 23
+console.log(primeiroNumero - segundoNumero)
+
+//multiplicação
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando
+console.log("O resultado da multiplicação é: " + produto)
+
+multiplicador = 8
+produto = multiplicador * multiplicando
+console.log("O resultado da multiplicação é: " + produto)
+
+//divisão
+let notaDoMercado = 50
+let pessoasParaDividir = 2
+console.log("O resultado da divisão: " + notaDoMercado / pessoasParaDividir)
+
+//resto da divisão
+let calculo = 10 % 3
+console.log("O modulo da divisão (resto) é: " + calculo)
