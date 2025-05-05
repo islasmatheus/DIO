38dc7992aa4974e010693e97238902eabfdc1986
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 10:42:01 -0300

    Else If

---
+++ Logica-de-programação/ElseIf.js
@@ -0,0 +1,9 @@
+// else if
+let nivelDeFome = 10
+if(nivelDeFome === 1){
+    console.log("Pouca fome")
+}else if (nivelDeFome === 2){
+    console.log("Muita fome")
+}else {
+    console.log("Você comeria mais que o pica-pau")
+}
