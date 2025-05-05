a2027525a328dd7b120020e20e39021f257c447c
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 10:05:44 -0300

    Maior e menor

---
+++ Logica-de-programação/MaiorEMenor.js
@@ -0,0 +1,9 @@
+let idadeMinima = 18
+let idadeUsuario = 18
+let idadePermitidaValida = idadeUsuario >= idadeMinima
+console.log(idadePermitidaValida)
+
+let idadeDeCorte = 50
+idadeUsuario = 50
+let resultadoEhTerceiraIDade = idadeDeCorte <= idadeUsuario
+console.log(resultadoEhTerceiraIDade)
