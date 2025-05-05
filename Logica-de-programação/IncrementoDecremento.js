f479775a8832ff1043f531045b84a67b03e90b50
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 09:31:19 -0300

    Incremento e decremento

---
+++ Logica-de-programação/IncrementoDecremento.js
@@ -0,0 +1,15 @@
+let contador = 1
+
+//incremento
+contador++
+console.log(contador)
+contador++
+console.log(contador)
+
+//decremento
+contador--
+console.log(contador)
+contador--
+console.log(contador)
+contador--
+console.log(contador)
