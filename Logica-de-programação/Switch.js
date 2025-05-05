a11194f872bd79dc413153eab199791176d5398a
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 10:50:56 -0300

    Switch Break Default

---
+++ Logica-de-programação/Switch.js
@@ -0,0 +1,15 @@
+let fruta = "Pêra"
+
+switch (fruta){
+    case "laranja":
+        console.log("Suco de laranja")
+            break
+    case "banana":
+        console.log("Vitamina de banana")
+            break
+    case "banana":
+        console.log("Suco de banana")
+            break
+    default:
+        console.log("Suco genérico")
+}
