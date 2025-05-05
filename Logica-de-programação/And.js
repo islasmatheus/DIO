1fe739090798a22cd144b34b94ed67481d9e95d5
Islas Matheus <devislasmatheus@gmail.com>
2025-05-05 10:13:23 -0300

    And

---
+++ Logica-de-programação/And.js
@@ -0,0 +1,10 @@
+// and (&&)
+let idade = 17
+let vistoVerificado = true
+let resultado = (idade >= 18) && (vistoVerificado === true)
+console.log(resultado)
+// and (&&) - 100 moedas coletadas e 1 item estrela
+let moedasColetadas = 100
+let item = "estrela"
+resultado = (moedasColetadas >= 100) && (item === "estrela")
+console.log(resultado)
