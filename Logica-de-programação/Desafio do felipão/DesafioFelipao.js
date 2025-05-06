581a6907010240dcb05be90b3c9ff29d4a2faf91
Islas Matheus <devislasmatheus@gmail.com>
2025-05-06 10:21:55 -0300

    Desafio felipão

---
+++ Logica-de-programação/Desafio do felipão/DesafioFelipao.js
@@ -0,0 +1,29 @@
+let nomeHeroi = "Islas"
+let nivel
+let xp = 10001
+
+if (xp <= 1000) {
+    nivel = "Ferro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else if (xp <= 2000) {
+    nivel = "Bronze"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else if (xp <= 5000) {
+    nivel = "Prata"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else if (xp <= 7000) {
+    nivel = "Ouro"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else if (xp <= 8000) {
+    nivel = "Platina"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else if (xp <= 9000) {
+    nivel = "Ascendente"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else if (xp <= 10000) {
+    nivel = "Imortal"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+} else {
+    nivel = "Radiante"
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
+}
