fd2febf5db2f6ae2726682420e82dd3dbac71a28
Islas Matheus <devislasmatheus@gmail.com>
2025-05-06 09:25:45 -0300

    For

---
+++ Logica-de-programação/For.js
@@ -0,0 +1,14 @@
+//for
+for(let i = 0; i < 4; i++){
+    console.log(i)
+    console.log("aumentando o contador")
+}
+
+let pontosDeVida = 0
+
+for(let y = 1; y <= 10; y++){
+    pontosDeVida +=1
+    console.log("Tomou poção mágica " + y)
+}
+
+console.log(pontosDeVida+" totais")
