01cc70c59b192b392f9eee5212c92830ade35ee1
Islas Matheus <devislasmatheus@gmail.com>
2025-05-07 09:09:21 -0300

    Função

---
+++ Logica-de-programação/Função.js
@@ -0,0 +1,11 @@
+torrar()
+
+function torrar(){
+    console.log("Torrando pão")
+    injetar()
+}
+
+function injetar(){
+    console.log("Preparando para injetar o pão")
+    console.log("Finalizado")
+}
