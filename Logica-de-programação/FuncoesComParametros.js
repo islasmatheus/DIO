44fe9bfd7d73bd763e24e8b9879b767dfe880725
Islas Matheus <devislasmatheus@gmail.com>
2025-05-08 08:53:41 -0300

    Função com retorno

--- Logica-de-programação/FuncoesComParametros.js
+++ Logica-de-programação/FuncoesComParametros.js
@@ -1,6 +1,8 @@
-torrar("Pão de forma")
+torrar("Pão de forma", "Felipe", 10.90)
 torrar("Pão integral")
 
-function torrar(pao){
+function torrar(pao, nome = "Cliente", valor = 0) {
     console.log("Torrada feita com " + pao)
+    console.log("Ela é um pedido de " + nome)
+    console.log("O valor total é " + valor)
 }
